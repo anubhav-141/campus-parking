05808afa1068764bf1ca5f0cb966009d8ed632ee
Raja Babu <rajababu8520456@gmail.com>
2026-04-23 01:36:04 +0530

    add jpa repositories

---
+++ src/main/java/parking/repository/NotificationRepository.java
@@ -0,0 +1,14 @@
+package parking.repository;
+
+import parking.model.Notification;
+import parking.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByUserOrderByCreatedAtDesc(User user);
+    List<Notification> findByUserAndIsReadFalseOrderByCreatedAtDesc(User user);
+    long countByUserAndIsReadFalse(User user);
+}
---
+++ src/main/java/parking/repository/ParkingZoneRepository.java
@@ -0,0 +1,11 @@
+package parking.repository;
+
+import parking.model.ParkingZone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ParkingZoneRepository extends JpaRepository<ParkingZone, Long> {
+    List<ParkingZone> findByStatus(ParkingZone.ZoneStatus status);
+}
